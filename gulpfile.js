1fc34dce95fe8c74524b78df443f62d6af01befd
Herrington Darkholme <nonamesheep1@gmail.com>
2015-08-05 16:27:08 +0800

    split source and source2

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,16 @@ var rename = require('gulp-rename');
 
 var p2bmapping = require('./template/p2b_mapping.json');
 var fontName = "baicons";
+var fontName2 = "baicons2";
 
 var fontPath = "fonts/";
 var cssDest = "./";
 
 var style = "baicons.css";
+var style2 = "baicons2.css";
 var p2b = "p2b.css";
 var reference = "icons-reference.html";
+var reference2 = "icons-reference2.html";
 
 function generateFonts(cb) {
   gulp.src(['source/*.svg'])
@@ -50,6 +53,36 @@ function generateFonts(cb) {
     .on('finish',cb);
 }
 
-gulp.task('build', generateFonts);
+function generateFonts2(cb) {
+  gulp.src(['source2/*.svg'])
+    .pipe(iconfont({
+      fontName: fontName2,
+	  normalize: true
+    }))
+    .on('glyphs', function (glyphs) {
 
-gulp.task('default', generateFonts);
+      gulp.src('template/styles_template.css')
+        .pipe(consolidate('lodash', {
+          glyphs: glyphs,
+          fontName: fontName2,
+          fontPath: fontPath
+        }))
+        .pipe(rename(style2))
+        .pipe(gulp.dest(cssDest));
+
+      gulp.src('template/reference_template.html')
+        .pipe(consolidate('lodash', {
+          glyphs: glyphs,
+          cssdest: cssDest,
+          style: style2
+        }))
+        .pipe(rename(reference2))
+        .pipe(gulp.dest(cssDest));
+    })
+    .pipe(gulp.dest('./fonts/'))
+    .on('finish',cb);
+}
+
+gulp.task('build', gulp.parallel(generateFonts, generateFonts2));
+
+gulp.task('default', gulp.parallel(generateFonts, generateFonts2));
